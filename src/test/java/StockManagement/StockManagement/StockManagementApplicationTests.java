5b2dbb85e579222bdd1daa3e069c616e6a9d1be9
Keane97 <liamkeane49@yahoo.com>
2022-05-30 13:59:04 +0100

    Initial commit

---
+++ src/test/java/StockManagement/StockManagement/StockManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package StockManagement.StockManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
